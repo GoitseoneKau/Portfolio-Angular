219bbcb19f0b8e2df59a23416ba16f6c4d3433c6
GoitseoneKau <66080430+GoitseoneKau@users.noreply.github.com>
2024-09-28 15:47:03 +0200

    =scroll button addition

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,68 +1,77 @@
-
-import { trigger, state, style, transition, animate } from '@angular/animations';
-import {  ApplicationRef, Component, OnInit, signal } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
+import { ApplicationRef, Component, OnInit, signal } from '@angular/core';
 import { first } from 'rxjs';
-
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
-  animations:  [
+  animations: [
     trigger('openClose', [
       transition('* => closed', [animate('5s')]),
       transition('* => open', [animate('5s')]),
     ]),
   ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
+  ProfessionText = signal('DEVELOPER'); //Text that is currently displayed
+  professionRoots: string[] = ['DEVELOPER', 'DESIGNER']; // Array of texts to type and delete
+  textIndex: number = 0; // Current text in the array
 
-  ProfessionText = signal("DEVELOPER") //Text that is currently displayed
-  professionRoots:string[] = ["DEVELOPER","DESIGNER"]      // Array of texts to type and delete
-  textIndex: number = 0             // Current text in the array
-
-  app:ApplicationRef; //initialize Application reference
+  app: ApplicationRef; //initialize Application reference
   charIndex: any;
+
+ 
+
+  constructor(appRef: ApplicationRef) {
+    //inject Apllication Reference
+    this.app = appRef;
+  }
+
+  ngOnInit(): void {
+    this.app.isStable.pipe(first((isStable) => isStable)).subscribe(() => {
+      this.startTyping();
+    });
+  }
+
+  startTyping() {
+    const currentString = this.professionRoots[this.textIndex];
+
+    if (this.charIndex < currentString.length) {
+      this.ProfessionText.update(
+        (value) => (value += currentString[this.charIndex])
+      );
+      this.charIndex++;
+      setTimeout(() => this.startTyping(), 200); // Typing speed
+    } else {
+      setTimeout(() => this.deleteText(), 1500); // Pause before deleting
+    }
+  }
+
+  deleteText() {
+    if (this.ProfessionText().length > 0) {
+      this.ProfessionText.set(this.ProfessionText().slice(0, -1));
+      setTimeout(() => this.deleteText(), 100); // Deleting speed
+    } else {
+      // Move to the next text
+      this.textIndex = (this.textIndex + 1) % this.professionRoots.length;
+      this.charIndex = 0;
+      setTimeout(() => this.startTyping(), 500); // Pause before typing again
+    }
+  }
+
+  scrollTo(element: HTMLElement) {
   
-    constructor(appRef : ApplicationRef){//inject Apllication Reference
-        this.app = appRef
-    }
-      
-    ngOnInit():void{
-      this.app.isStable.pipe(
-          first((isStable) => isStable))
-        .subscribe(() => { this.startTyping(); });
-    }
-
-    startTyping() {
-      const currentString = this.professionRoots[this.textIndex];
+      element.scrollIntoView({ behavior: 'smooth', inline: 'nearest' });
   
-      if (this.charIndex < currentString.length) {
-        this.ProfessionText.update(value=>value += currentString[this.charIndex]);
-        this.charIndex++;
-        setTimeout(() => this.startTyping(), 200); // Typing speed
-      } else {
-        setTimeout(() => this.deleteText(), 1500); // Pause before deleting
-      }
-    }
   
-    deleteText() {
-      if (this.ProfessionText().length > 0) {
-        this.ProfessionText.set( this.ProfessionText().slice(0, -1));
-        setTimeout(() => this.deleteText(), 100); // Deleting speed
-      } else {
-        // Move to the next text
-        this.textIndex = (this.textIndex + 1) % this.professionRoots.length;
-        this.charIndex = 0;
-        setTimeout(() => this.startTyping(), 500); // Pause before typing again
-      }
-    }
-
-
-scrollTo(element:HTMLElement){
-  element.scrollIntoView({ behavior: "smooth",  inline: "nearest" })
-}
-
+  }
 }
